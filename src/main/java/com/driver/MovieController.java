2895fa6ca88c6685d921786ae1cdde9767e5a294
jeeva <jeevanthevar@gmail.com>
2022-11-29 13:16:37 +0530

    Third update

--- src/main/java/com/driver/MovieController.java
+++ src/main/java/com/driver/MovieController.java
@@ -3,13 +3,14 @@ package com.driver;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
-
+@Component
 public class MovieController {
 
     @Autowired
